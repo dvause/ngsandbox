1cf5f203abfb5963432b5a8eae64179869a86caf
David Vause <dvause@gmail.com>
2014-02-13 21:08:01 -0500

    implement external rest service. having CORS issues

--- src/app/hello/hello.js
+++ src/app/hello/hello.js
@@ -2,7 +2,9 @@ angular.module('ngsandbox.hello', [
   'ui.router'
 ])
 
-  .config(function ($stateProvider) {
+
+
+  .config(function ($stateProvider, $httpProvider) {
     $stateProvider
       .state('main.hello', {
         url: '/',
@@ -10,11 +12,24 @@ angular.module('ngsandbox.hello', [
         controller: 'HelloController as hello'
       })
     ;
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
 
-  .controller('HelloController', function HelloController() {
+  .controller('HelloController', function HelloController(HelloService) {
     var helloCtrl = this;
     helloCtrl.hello = "Hello, World!";
+//    helloCtrl.hello = HelloService.sayHello();
+  })
+
+  .service('HelloService', function HelloService($http, $q) {
+    var helloService = this;
+
+    helloService.sayHello = function() {
+      return $http.get('http://localhost:8080/spring-data-rest-service').then(function(result) {
+        return result;
+      })
+    }
   })
 
 ;
